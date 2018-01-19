9c174ee172a0e9e32abcac92aa9d02c43ca8a5b5
Chris Cornwell <chris@citizennet.com>
2018-01-19 14:42:19 -0800

    refactor dropdown component to be compatible with arbitrary data types

--- src/Dropdown/Dropdown.purs
+++ src/Dropdown/Dropdown.purs
@@ -1,14 +1,13 @@
 module CN.UI.Dropdown where
 
 import Prelude
-import Select.Dispatch
 
-import Control.Monad.Aff.Console (log)
 import Data.Array ((:), difference, mapWithIndex)
-import Data.Maybe (Maybe(..))
+import Data.Maybe (Maybe(Nothing))
 import Halogen as H
 import Halogen.HTML as HH
 import Halogen.HTML.Events as HE
+import Select.Dispatch (ContainerQuery(..), ContainerState, Dispatch(..), emit, getContainerProps, getItemProps, getToggleProps)
 import Select.Effects (FX)
 import Select.Primitive.Container as C
 
@@ -30,85 +29,88 @@ data SelectableStatus
 -- Component types
 
 -- Component state definition
-type State =
-  { items :: Array DropdownItem
-  , selections :: Array DropdownItem }
+type State item e =
+  { items :: Array item
+  , selections :: Array item
+  , itemHTML :: item -> Array (H.HTML Void (ChildQuery item e))
+  }
 
 -- Component query definition
-data Query e a
-  = HandleContainer (C.Message String (Query e) e) a
+data Query item e a
+  = HandleContainer (C.Message item (Query item e) e) a
 
 -- Component top-level definition
-type DropdownComponent e
-  = H.Component HH.HTML (Query e) DropdownInput DropdownMessage (FX e)
+type DropdownComponent item e
+  = H.Component HH.HTML (Query item e) (DropdownInput item e) DropdownMessage (FX e)
 
 -- Component input and message types
-type DropdownInput =
-  { items :: Array DropdownItem }
+type DropdownInput item e =
+  { items :: Array item
+  , itemHTML :: item -> Array (H.HTML Void (ChildQuery item e))
+  }
 
 type DropdownMessage = Void
 
 -- Component child types
-type ChildQuery e = Dispatch DropdownItem (Query e) e
+type ChildQuery item e = Dispatch item (Query item e) e
 type ChildSlot = Unit
 
 -- Return type of render function; must use Dispatch as child type (or coproduct)
-type DropdownHTML e =
-  H.ParentHTML (Query e) (ChildQuery e) ChildSlot (FX e)
+type DropdownHTML item e =
+  H.ParentHTML (Query item e) (ChildQuery item e) ChildSlot (FX e)
 
 -- Return type of eval function
-type DropdownDSL e =
-  H.ParentDSL State (Query e) (ChildQuery e) ChildSlot DropdownMessage (FX e)
+type DropdownDSL item e =
+  H.ParentDSL (State item e) (Query item e) (ChildQuery item e) ChildSlot DropdownMessage (FX e)
 
 
 
 ----------
 -- Component definition
-component :: ∀ e. DropdownComponent e
+component :: ∀ item e. Eq item => DropdownComponent item e
 component =
   H.parentComponent
-    { initialState: \i -> { items: i.items, selections: [] }
+    { initialState: \i -> { items: i.items, selections: [], itemHTML: i.itemHTML }
     , render
     , eval
     , receiver: const Nothing
     }
   where
-    render :: State -> DropdownHTML e
+    render :: State item e -> DropdownHTML item e
     render st =
       HH.div_
-      [ HH.slot
-          unit
-          C.component
-          { items: st.items, render: renderContainer }
-          ( HE.input HandleContainer )
-      ]
+        [ HH.slot
+            unit
+            C.component
+            { items: st.items, render: renderContainer st.itemHTML }
+            ( HE.input HandleContainer )
+        ]
 
-    eval :: (Query e) ~> (DropdownDSL e)
+    eval :: (Query item e) ~> (DropdownDSL item e)
     eval = case _ of
 
       HandleContainer m a -> case m of
         C.Emit q -> emit eval q a
 
         C.ItemSelected item -> do
-           H.liftAff $ log ("Selected: " <> item)
-           H.modify \st -> st { selections = item : st.selections }
+          H.modify \st -> st { selections = item : st.selections }
 
-           st <- H.get
-           _ <- H.query unit
-                  $ H.action
-                  $ Container
-                  $ ContainerReceiver
-                  $ { render: renderContainer, items: difference st.items st.selections }
+          st <- H.get
+          _ <- H.query unit
+            $ H.action
+            $ Container
+            $ ContainerReceiver
+            $ { render: renderContainer st.itemHTML, items: difference st.items st.selections }
 
-           pure a
+          pure a
 
 
 ----------
 -- Render helpers
 
 -- Render the dropdown
-renderContainer :: ∀ e. (ContainerState String) -> H.HTML Void (ChildQuery e)
-renderContainer st =
+renderContainer :: ∀ item e. (item -> Array (H.HTML Void (ChildQuery item e))) -> (ContainerState item) -> H.HTML Void (ChildQuery item e)
+renderContainer itemHTML st =
   HH.div_
   $ if not st.open
     then [ renderToggle ]
@@ -117,20 +119,20 @@ renderContainer st =
   where
 
     -- The clickable region that opens the dropdown
-    renderToggle :: H.HTML Void (ChildQuery e)
+    renderToggle :: H.HTML Void (ChildQuery item e)
     renderToggle =
       HH.span
       ( getToggleProps [] )
       [ HH.text "Toggle" ]
 
     -- The individual items to render
-    renderItems :: Array (H.HTML Void (ChildQuery e)) -> H.HTML Void (ChildQuery e)
+    renderItems :: Array (H.HTML Void (ChildQuery item e)) -> H.HTML Void (ChildQuery item e)
     renderItems html =
       HH.div
       ( getContainerProps [] )
       [ HH.ul_ html ]
 
     -- One particular item to render
-    renderItem :: Int -> DropdownItem -> H.HTML Void (ChildQuery e)
+    renderItem :: Int -> item -> H.HTML Void (ChildQuery item e)
     renderItem index item =
-      HH.li ( getItemProps index [] ) [ HH.text item ]
+      HH.li ( getItemProps index [] ) $ itemHTML item
