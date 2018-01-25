0c68ba4117e003c1fb69c84b8c9924092e35ac07
Thomas R. Honeyman <admin@thomashoneyman.com>
2018-01-24 17:12:04 -0800

    Added Tailwind and config.

--- test/Component.purs
+++ test/Component.purs
@@ -4,14 +4,10 @@ import Prelude
 
 import CN.UI.Dropdown as Dropdown
 import CN.UI.Typeahead as Typeahead
-import Data.Either.Nested (Either2)
-import Data.Functor.Coproduct.Nested (Coproduct2)
 import Data.Maybe (Maybe(..))
+import Data.Tuple (Tuple(Tuple))
 import Halogen as H
-import Halogen.Component.ChildPath (cp1)
-import Halogen.Component.ChildPath as CP
 import Halogen.HTML as HH
-import Halogen.HTML.Events as HE
 import Halogen.HTML.Properties as HP
 import Select.Effects (FX)
 
@@ -32,7 +28,6 @@ data Query a
 
 type ChildQuery = Typeahead.Query
 type ChildSlot = Unit
-
 
 ----------
 -- Item specializations
@@ -59,18 +54,9 @@ component =
     -- For the sake of testing and visual demonstration, we'll just render
     -- out a bunch of selection variants in respective slots
     render :: State -> HTML e
-    render st =
-      HH.section
-      [ HP.class_ $ HH.ClassName "mw6 mw7-ns center pa2 ph4-ns black-80 sans-serif" ]
-      [ title
-      , introduction
-      , mountWith
-          "Typeahead"
-          "Captures string input and produces a menu."
-          ( HH.slot unit Typeahead.component { items: containerData } absurd )
-      ]
+    render st = container cnNavSections cnDocumentationBlocks
 
-    eval :: Query ~> H.ParentDSL State Query _ _ _ (FX e)
+    eval :: Query ~> H.ParentDSL State Query ChildQuery ChildSlot Void (FX e)
     eval (NoOp next) = pure next
     eval (HandleDropdown _ next) = pure next
 
@@ -86,34 +72,138 @@ containerData =
 
 
 ----------
--- CSS helpers
+-- HTML
 
-cssTitle = HP.class_ $ HH.ClassName "f1 lh-title"
-cssExampleTitle = HP.class_ $ HH.ClassName "f2 lh-title"
-cssExampleDescription = HP.class_ $ HH.ClassName "f4 lh-copy black-70"
-cssIntroText = HP.class_ $ HH.ClassName "f4 lh-copy"
-cssExampleBlock = HP.class_ $ HH.ClassName "pa4 bg-near-white"
-cssPullQuote = HP.class_ $ HH.ClassName "pl4 mb4 bl bw2 b--blue f5 lh-copy"
-cssPostScript = HP.class_ $ HH.ClassName "f5 mt4 lh-copy black-60 i"
+css :: ∀ t0 t1. String -> H.IProp ( "class" :: String | t0 ) t1
+css = HP.class_ <<< HH.ClassName
 
-title :: ∀ e. HTML e
-title =
-  HH.h1
-  [ cssTitle ]
-  [ HH.text "CitizenNet Select Components" ]
-
-introduction :: ∀ e. HTML e
-introduction =
-  HH.p
-  [ cssIntroText ]
-  [ HH.text "A demonstration of various pre-built selection components ready to be dropped in to Wildcat screens. This can be used to verify that components behave and look as expected & required." ]
-
--- mountWith :: ∀ e. String -> _ -> String -> HTML e
-mountWith title body slot =
-  HH.div
-  [ ]
-  [ HH.h2 [ cssExampleTitle ] [ HH.text title ]
-  , HH.p  [ cssExampleDescription ] [ HH.text body ]
-  , HH.div [ cssExampleBlock ] [ slot ]
+container :: ∀ e
+  . Array (Tuple String (Array (Tuple String String)))
+ -> Array (HTML e)
+ -> HTML e
+container navs blocks =
+  HH.body
+  [ css "font-sans font-normal text-black leading-normal" ]
+  [ HH.div
+    [ css "min-h-screen" ]
+    [ sidebar navs
+    , innerContainer "CitizenNet UI Guide" blocks
+    ]
   ]
 
+innerContainer :: ∀ i p. String -> Array (H.HTML i p) -> H.HTML i p
+innerContainer title blocks =
+  HH.div
+  [ css "md:ml-80" ]
+  [ HH.div
+    [ css "fixed w-full z-20" ]
+    [ HH.div
+      [ css "pin-t bg-white md:hidden relative border-b border-grey-light h-12 flex items-center" ]
+      [ HH.a
+        [ css "mx-auto inline-flex items-center"
+        , HP.href "#" ]
+        [ HH.text title ]
+      ]
+    ]
+  , HH.div
+    [ css "px-6 pb-8 pt-20 md:pt-16 w-full max-w-lg mx-auto" ]
+    blocks
+  ]
+
+cnDocumentationBlocks :: ∀ e. Array (HTML e)
+cnDocumentationBlocks = typeaheadBlock <> []
+
+typeaheadBlock :: ∀ e. Array (HTML e)
+typeaheadBlock = documentationBlock
+  "Typeahead"
+  "Uses string input to search pre-determined entries."
+  ( componentBlock "No configuration set." slot )
+  where
+    slot = HH.slot unit Typeahead.component { items: containerData } absurd
+
+documentationBlock :: ∀ i p. String -> String -> H.HTML i p -> Array (H.HTML i p)
+documentationBlock title description block =
+  [ HH.h1_ [ HH.text title ]
+  , HH.div [ css "text-xl text-grey-dark mb-4" ] [ HH.text description ]
+  , block ]
+
+componentBlock :: ∀ e. String -> HTML e -> HTML e
+componentBlock config slot =
+  HH.div
+  [ css "rounded overflow-hidden border-2 border-grey-light mb-8 bg-white" ]
+  [ HH.div [ css "border-b-2 border-grey-light p-4" ] [ mkConfig config ]
+  , HH.div [ css "p-4 bg-grey-lightest" ] [ slot ]
+  ]
+  where
+    mkConfig :: ∀ i p. String -> H.HTML i p
+    mkConfig str = HH.p_ [ HH.text str ]
+
+sidebar :: ∀ i p. Array (Tuple String (Array (Tuple String String))) -> H.HTML i p
+sidebar navSections =
+  HH.div
+  [ HP.id_ "sidebar"
+  , css "hidden z-50 fixed pin-y pin-l overflow-y-scroll md:overflow-visible scrolling-touch md:scrolling-auto bg-grey-lighter w-4/5 md:w-full md:max-w-xs flex-none border-r-2 border-grey-light md:flex flex-col" ]
+  [ HH.div
+    [ css "p-8 flex-1 overflow-y-scroll" ]
+    [ HH.nav
+      [ HP.id_ "nav"
+      , css "text-base overflow-y-scroll" ]
+      ( navSection <$> navSections )
+    ]
+  ]
+  where
+    navSection :: Tuple Title (Array (Tuple Anchor Link)) -> H.HTML i p
+    navSection (Tuple title items) =
+      HH.div
+      [ css "mb-8" ]
+      [ HH.p
+        [ css "mb-4 text-grey-dark uppercase tracking-wide font-bold text-xs" ]
+        [ HH.text title ]
+      , HH.ul
+        [ css "list-reset" ]
+        ( navLink <$> items )
+      ]
+      where
+        navLink (Tuple anchor link) =
+          HH.li
+          [ css "mb-3" ]
+          [ HH.a
+            [ css "hover:underline text-grey-darkest"
+            , HP.href link ]
+            [ HH.text anchor ]
+          ]
+
+type Title = String
+type Anchor = String
+type Link = String
+
+cnNavSections :: Array (Tuple Title (Array (Tuple Anchor Link)))
+cnNavSections = [ intro, global, components ]
+  where
+    mkNavSection :: Title -> Array Anchor -> Tuple Title (Array (Tuple Anchor Link))
+    mkNavSection title xs = Tuple title links
+      where
+        links :: Array (Tuple Anchor Link)
+        links = (\a -> Tuple a "#") <$> xs
+
+    intro = mkNavSection "Introduction" [ "What is this?" ]
+
+    global = mkNavSection "Global"
+      [ "Grid"
+      , "Typography"
+      , "Colors" ]
+
+    components = mkNavSection "Components"
+      [ "Frame"
+      , "Text Fields"
+      , "Buttons"
+      , "Dialogs"
+      , "Hovercards"
+      , "Filters"
+      , "Sorting"
+      , "Modals"
+      , "Menus"
+      , "Toasts"
+      , "Paging"
+      , "Loading"
+      , "Validation" ]
