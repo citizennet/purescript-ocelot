19f7a762a22fef259db25a8a39872b55325bfe2d
Thomas R. Honeyman <admin@thomashoneyman.com>
2018-01-25 14:55:45 -0800

    Minify CSS with OptimizeCssAssets and and PurgeCSS

--- css/webpack.config.js
+++ css/webpack.config.js
@@ -1,5 +1,18 @@
-const path = require('path');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
+let path = require('path');
+let glob = require('glob-all');
+let ExtractTextPlugin = require('extract-text-webpack-plugin');
+let PurgecssPlugin = require('purgecss-webpack-plugin');
+let OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+
+
+// Custom PurgeCSS extractor for Tailwind that allows special characters in
+// class names.
+// https://github.com/FullHuman/purgecss#extractor
+class TailwindExtractor {
+  static extract(content) {
+    return content.match(/[A-z0-9-:\/]+/g) || [];
+  }
+}
 
 module.exports = {
   entry: './index.js',
@@ -15,6 +28,7 @@ module.exports = {
           fallback: 'style-loader',
           use: [
             { loader: 'css-loader', options: { importLoaders: 1 } },
+						// Use for Tailwind processing
             'postcss-loader'
           ]
         })
@@ -22,6 +36,27 @@ module.exports = {
     ]
   },
   plugins: [
-    new ExtractTextPlugin('cn-tailwind.css')
-  ]
+		// Bundle CSS to separate file, not JS
+    new ExtractTextPlugin('cn-tailwind.css'),
+
+		// Remove class names not used in any source files
+		new	PurgecssPlugin({
+			// Locations of any files to scan
+			paths: glob.sync([
+				path.join(__dirname, "../src/**/*.purs"),
+				path.join(__dirname, "../test/**/*.purs"),
+				path.join(__dirname, "../docs/**/*.js"),
+			]),
+			extractors: [
+				{
+					extractor: TailwindExtractor,
+					extensions: [ "js", "purs" ]
+				}
+			]
+		}),
+
+		// Minify the CSS after processing
+		new OptimizeCssAssetsPlugin()
+
+	]
 }
