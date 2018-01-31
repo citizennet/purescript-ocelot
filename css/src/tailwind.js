a52cfc7d0f4f9d915f3e5daa6e2dbdf31e594815
Forest Loren Toney III <foresttoney@gmail.com>
2018-01-31 12:49:23 -0800

    feature: Add button block
    FBCM-2115

--- css/src/tailwind.js
+++ css/src/tailwind.js
@@ -21,6 +21,19 @@ config.margin = Object.assign(config.margin, {
 
 config.negativeMargin = config.margin
 
+config.colors = Object.assign(config.colors, {
+  'black': '#333',
+  'grey-darkest': '#666',
+  'grey-darker': '#999',
+  'grey-dark': 'rgba(0,0,0,0.35)',
+  'grey': '#BFBFBF',
+  'grey-light': 'rgba(128,128,128,0.2)',
+  'grey-lighter': '#E6E6E6',
+  'grey-lightest': '#F5F5F5',
+  'white': '#FFF',
+  'teal': '#0BB6D9',
+  'teal-dark': '#00A3CC'
+})
 
 // Export the new configuration
 module.exports = config
