4520e05590c9e5deccd8654ce7fc870eb4d4f25a
Forest Loren Toney III <foresttoney@gmail.com>
2018-01-31 23:11:35 -0800

    refactor: Add custom utility classes for pseudo selectors

--- src/Blocks/Input.purs
+++ src/Blocks/Input.purs
@@ -6,12 +6,23 @@ import DOM.HTML.Indexed (HTMLinput)
 import Halogen.HTML as HH
 import Halogen.HTML.Properties as HP
 
-cnInput :: HH.ClassName
-cnInput = HH.ClassName "cn-input"
+inputClasses :: Array HH.ClassName
+inputClasses = HH.ClassName <$>
+  [ "bg-white"
+  , "cc-teal"
+  , "disabled:bg-grey-light"
+  , "focus:no-outline"
+  , "font-light"
+  , "px-4"
+  , "py-2"
+  , "rounded-sm"
+  , "text-sm"
+  , "w-full"
+  ]
 
 input
   :: ∀ p i
   . Array (HH.IProp HTMLinput i)
   -> HH.HTML p i
 input iprops =
-  HH.input (iprops <> [ HP.class_ cnInput ])
+  HH.input (iprops <> [ HP.classes inputClasses ])
