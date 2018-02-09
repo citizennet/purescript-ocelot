cf84f812ea27848cb1e6764d4cf83451944a4f45
Thomas R. Honeyman <admin@thomashoneyman.com>
2018-02-09 14:21:13 -0800

    Restore bower

--- ui-guide/Main.purs
+++ ui-guide/Main.purs
@@ -2,12 +2,19 @@ module Main where
 
 import Prelude
 import UIGuide.Components.TextFields as TextFields
+import UIGuide.Router (Stories, runStorybook, proxy)
 
 import Control.Monad.Eff (Eff)
 import Halogen.Aff as HA
-import Halogen.VDom.Driver (runUI)
 
-main :: forall eff. Eff (HA.HalogenEffects (TextFields.Effects eff)) Unit
+import Data.StrMap as SM
+import Data.Tuple (Tuple(..))
+
+stories = SM.fromFoldable
+  [ Tuple "textfields" $ proxy $ TextFields.component
+  ]
+
+main :: ∀ eff. Eff (HA.HalogenEffects (TextFields.Effects eff)) Unit
 main = HA.runHalogenAff do
   body <- HA.awaitBody
-  runUI TextFields.component unit body
+  runStorybook stories body
