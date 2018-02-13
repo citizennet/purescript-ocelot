7e2f91d802e8012b199b60588cdb6e16ae700d98
Forest Loren Toney III <foresttoney@gmail.com>
2018-02-12 22:54:14 -0800

    refactor: Move tabs to seperate component

--- ui-guide/App/Routes.purs
+++ ui-guide/App/Routes.purs
@@ -10,11 +10,13 @@ import Data.Tuple (Tuple(..))
 
 import UIGuide.Components.Button as Button
 import UIGuide.Components.FormControl as FormControl
+import UIGuide.Components.Tab as Tab
 import UIGuide.Components.TextFields as TextFields
 
 routes = SM.fromFoldable
   [ Tuple "Text Fields" $ proxy $ TextFields.component
   , Tuple "Buttons" $ proxy $ Button.component
   , Tuple "Form Controls" $ proxy $ FormControl.component
+  , Tuple "Tabs" $ proxy $ Tab.component
   ]
 
