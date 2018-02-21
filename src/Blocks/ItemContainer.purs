f589f1bbde91044c8e82a20d059481bea0030ac3
Thomas R. Honeyman <admin@thomashoneyman.com>
2018-02-20 19:05:54 -0800

    Add base container components, handling attaching props for you.

---
+++ src/Blocks/ItemContainer.purs
@@ -0,0 +1,124 @@
+module CN.UI.Block.ItemContainer (itemContainer, selectionContainer, selectionGroup, boldMatches) where
+
+import Prelude
+
+import Data.FunctorWithIndex (mapWithIndex)
+import Data.Fuzzy (Fuzzy(..))
+import Data.Maybe (Maybe(..), fromMaybe)
+import Data.StrMap (lookup)
+import Data.Monoid (mempty)
+import Data.Either (Either(..))
+import Halogen as H
+import Halogen.HTML as HH
+import Halogen.HTML.Properties as HP
+import Select.Primitives.Container as C
+
+baseClasses :: Array HH.ClassName
+baseClasses = HH.ClassName <$>
+  [ "bg-white"
+  , "rounded-sm"
+  , "w-full" ]
+
+selectionContainerClasses :: Array HH.ClassName
+selectionContainerClasses = baseClasses <> names
+  where
+    names = HH.ClassName <$>
+      [ "border-b"
+      , "border-grey-lighter"
+      ]
+
+itemContainerClasses :: Array HH.ClassName
+itemContainerClasses = baseClasses <> names
+  where
+    names = HH.ClassName <$>
+      [ "absolute"
+      , "shadow"
+      , "max-h-80"
+      , "overflow-y-scroll"
+      , "pin-t"
+      , "pin-l"
+      , "z-50"
+      ]
+
+ulClasses :: Array HH.ClassName
+ulClasses = HH.ClassName <$> [ "list-reset" ]
+
+liClasses :: Array HH.ClassName
+liClasses = HH.ClassName <$> [ "p-1" ]
+
+groupClasses :: Array HH.ClassName
+groupClasses = HH.ClassName <$>
+  [ "group"
+  , "leading-normal"
+  , "hover:bg-grey-lightest"
+  , "px-3"
+  , "py-1"
+  ]
+
+buttonClasses :: Array HH.ClassName
+buttonClasses = HH.ClassName <$>
+  [ "text-grey-light"
+  , "float-right"
+  , "leading-normal"
+  , "invisible"
+  , "group-hover:visible"
+  ]
+
+-- Provided an array of items, renders them in a container.
+-- Items should have already been treated with `boldMatches` by this point.
+itemContainer :: ∀ t o item. Maybe Int -> Array HH.PlainHTML -> HH.HTML t (C.ContainerQuery o item Unit)
+itemContainer highlightIndex html =
+  HH.div
+  (C.getContainerProps [ HP.classes itemContainerClasses ])
+  [ HH.ul
+    [ HP.classes ulClasses ]
+    $ mapWithIndex
+      ( \i h ->
+          HH.li
+            ( C.getItemProps i
+              [ HP.classes (liClasses <> hover i) ]
+            )
+            [ HH.fromPlainHTML h ]
+      )
+      html
+  ]
+  where
+    hover i = if highlightIndex == Just i then HH.ClassName <$> [ "bg-grey-lighter" ] else mempty
+
+
+-- Provided an array of selection items, renders them in a container
+-- Make sure the array of items includes the correct click handlers:w
+selectionContainer :: ∀ p i. Array (HH.HTML p i) -> HH.HTML p i
+selectionContainer html =
+  HH.div
+  [ HP.classes selectionContainerClasses ]
+  [ HH.ul
+    [ HP.classes ulClasses ]
+    $ html <#>
+    ( \h ->
+        HH.li
+          [ HP.classes liClasses ]
+					[ h ]
+    )
+  ]
+
+
+selectionGroup :: ∀ item i p. (item -> HH.PlainHTML) -> Array (HH.IProp _ p) -> item -> HH.HTML i p
+selectionGroup f props item =
+	HH.div
+		( props <> [ HP.classes groupClasses ] )
+		[ HH.fromPlainHTML (f item)
+		, HH.button
+			[ HP.classes buttonClasses ]
+			[ HH.text "x" ]
+		]
+
+
+-- Takes a key to the segment that you want to display highlighted.
+-- WARN: If the key you provided does not exist in the map, your item will not be
+-- rendered!
+boldMatches :: ∀ item i p. String -> Fuzzy item -> Array (H.HTML i p)
+boldMatches key (Fuzzy { segments }) = boldMatch <$> (fromMaybe [ Left key ] $ lookup key segments)
+  where
+    boldMatch (Left str) = HH.text str
+    boldMatch (Right str) = HH.span [ HP.class_ $ HH.ClassName "font-bold" ] [ HH.text str ]
--- src/Components/Typeahead/Typeahead.purs
+++ src/Components/Typeahead/Typeahead.purs
@@ -2,24 +2,22 @@ module CN.UI.Components.Typeahead where
 
 import Prelude
 
-import Data.Either (Either(..))
-import Data.Fuzzy (Fuzzy(..))
-import Data.Maybe (Maybe(..), fromMaybe)
-import Data.StrMap (StrMap, lookup)
+import Data.Fuzzy (Fuzzy)
+import Data.Maybe (Maybe(Nothing, Just))
+import Data.StrMap (StrMap)
 import CN.UI.Core.Typeahead as TA
 import Control.Monad.Aff.Class (class MonadAff)
-import Data.Array (dropEnd, mapWithIndex, takeEnd)
-import Data.Foldable (foldr)
 import Data.Time.Duration (Milliseconds(..))
 import Halogen as H
 import Halogen.Component.ChildPath as CP
 import Halogen.HTML as HH
 import Halogen.HTML.Events as HE
 import Halogen.HTML.Properties as HP
-import Network.RemoteData (RemoteData(..), withDefault)
+import Network.RemoteData (RemoteData(NotAsked))
 import Select.Primitives.Container as C
 import Select.Primitives.Search as S
 
+import CN.UI.Block.ItemContainer as ItemContainer
 import CN.UI.Block.Input as Input
 
 ----------
@@ -32,8 +30,8 @@ defaultSingle :: ∀ o item source err eff m
  => Show err
  => Array item
  -> (item -> StrMap String)
- -> RenderContainerRow o item
- -> (item -> TAParentHTML o item source err (TA.Effects eff) m)
+ -> (Fuzzy item -> HH.PlainHTML)
+ -> (item -> HH.PlainHTML)
  -> TA.TypeaheadInput o item source err (TA.Effects eff) m
 defaultSingle xs toStrMap renderFuzzy renderItem =
   { items: TA.Sync xs
@@ -51,8 +49,8 @@ defaultLimit :: ∀ o item source err eff m
  => Int
  -> Array item
  -> (item -> StrMap String)
- -> RenderContainerRow o item
- -> (item -> TAParentHTML o item source err (TA.Effects eff) m)
+ -> (Fuzzy item -> HH.PlainHTML)
+ -> (item -> HH.PlainHTML)
  -> TA.TypeaheadInput o item source err (TA.Effects eff) m
 defaultLimit n xs toStrMap renderFuzzy renderItem =
   { items: TA.Sync xs
@@ -70,8 +68,8 @@ defaultMulti :: ∀ o item source err eff m
  => Show err
  => Array item
  -> (item -> StrMap String)
- -> RenderContainerRow o item
- -> (item -> TAParentHTML o item source err (TA.Effects eff) m)
+ -> (Fuzzy item -> HH.PlainHTML)
+ -> (item -> HH.PlainHTML)
  -> TA.TypeaheadInput o item source err (TA.Effects eff) m
 defaultMulti xs toStrMap renderFuzzy renderItem =
   { items: TA.Sync xs
@@ -88,8 +86,8 @@ defaultAsyncSingle :: ∀ o item source err eff m
   => Show err
   => source
   -> (item -> StrMap String)
-  -> RenderContainerRow o item
-  -> (item -> TAParentHTML o item source err (TA.Effects eff) m)
+  -> (Fuzzy item -> HH.PlainHTML)
+  -> (item -> HH.PlainHTML)
   -> TA.TypeaheadInput o item source err (TA.Effects eff) m
 defaultAsyncSingle source toStrMap renderFuzzy renderItem =
   { items: TA.Async source NotAsked
@@ -106,8 +104,8 @@ defaultAsyncMulti :: ∀ o item source err eff m
  => Show err
  => source
  -> (item -> StrMap String)
- -> RenderContainerRow o item
- -> (item -> TAParentHTML o item source err (TA.Effects eff) m)
+ -> (Fuzzy item -> HH.PlainHTML)
+ -> (item -> HH.PlainHTML)
  -> TA.TypeaheadInput o item source err (TA.Effects eff) m
 defaultAsyncMulti source toStrMap renderFuzzy renderItem =
   { items: TA.Async source NotAsked
@@ -125,8 +123,8 @@ defaultContAsyncMulti :: ∀ o item source err eff m
  => Show err
  => source
  -> (item -> StrMap String)
- -> RenderContainerRow o item
- -> (item -> TAParentHTML o item source err (TA.Effects eff) m)
+ -> (Fuzzy item -> HH.PlainHTML)
+ -> (item -> HH.PlainHTML)
  -> TA.TypeaheadInput o item source err (TA.Effects eff) m
 defaultContAsyncMulti source toStrMap renderFuzzy renderItem =
   { items: TA.ContinuousAsync (Milliseconds 500.0) "" source NotAsked
@@ -186,8 +184,8 @@ type TAParentHTML o item source err eff m
 renderTA :: ∀ o item source err eff m
   . MonadAff (TA.Effects eff) m
  => Eq item
- => RenderContainerRow o item
- -> (item -> TAParentHTML o item source err (TA.Effects eff) m)
+ => (Fuzzy item -> HH.PlainHTML)
+ -> (item -> HH.PlainHTML)
  -> TA.TypeaheadState item source err
  -> TAParentHTML o item source err (TA.Effects eff) m
 renderTA renderFuzzy renderSelection st =
@@ -209,99 +207,31 @@ renderTA renderFuzzy renderSelection st =
       CP.cp1
       TA.ContainerSlot
       C.component
-      { render: renderContainer st, items: [] }
+      { render: renderContainer, items: [] }
       (HE.input TA.HandleContainer)
   ]
   where
     renderSelections =
       case st.selections of
         (TA.One Nothing) -> HH.div_ []
-        (TA.One (Just x)) -> HH.div_
-          [ HH.div
-            [ HP.class_ $ HH.ClassName "bg-white rounded-sm w-full text-grey-darkest border-b border-grey-lighter" ]
-            [ HH.ul
-              [ HP.class_ $ HH.ClassName "list-reset" ]
-              [ renderSelection x ]
-            ]
-          ]
-        (TA.Many xs) -> HH.div_
-          [ HH.div
-            [ HP.class_ $ HH.ClassName "bg-white rounded-sm w-full text-grey-darkest border-b border-grey-lighter" ]
-            [ HH.ul
-              [ HP.class_ $ HH.ClassName "list-reset" ]
-              $ renderSelection <$> xs
-            ]
-          ]
-        (TA.Limit _ xs) -> HH.div_
-          [ HH.div
-            [ HP.class_ $ HH.ClassName "bg-white rounded-sm w-full text-grey-darkest border-b border-grey-lighter z-50" ]
-            [ HH.ul
-              [ HP.class_ $ HH.ClassName "list-reset" ]
-              $ renderSelection <$> xs
-            ]
-          ]
+        (TA.One (Just x)) ->  HH.div_ [ ItemContainer.selectionContainer [ ItemContainer.selectionGroup renderSelection (props x) x ] ]
+        (TA.Many xs) -> HH.div_ [ ItemContainer.selectionContainer $ (\x -> ItemContainer.selectionGroup renderSelection (props x) x) <$> xs ]
+        (TA.Limit _ xs) -> HH.div_ [ ItemContainer.selectionContainer $ (\x -> ItemContainer.selectionGroup renderSelection (props x) x) <$> xs ]
+			where
+				props item = [ HE.onClick (HE.input_ (TA.Remove item)) ]
 
-    renderContainer parentSt containerState = HH.div [ HP.class_ $ HH.ClassName "relative" ]
-      if not containerState.open
+    renderContainer cst = HH.div [ HP.class_ $ HH.ClassName "relative" ]
+      if not cst.open
         then []
-        else [ HH.div
-          ( C.getContainerProps
-            [ HP.class_ $ HH.ClassName "absolute bg-white shadow max-h-80 overflow-y-scroll rounded-sm pin-t pin-l w-full z-50" ]
-          )
-          [ HH.ul
-            [ HP.class_ $ HH.ClassName "list-reset" ]
-            ( renderFuzzy containerState.highlightedIndex `mapWithIndex` containerState.items )
-          ]
-        ]
+        else [ ItemContainer.itemContainer cst.highlightedIndex (renderFuzzy <$> cst.items) ]
 
-    renderSearch searchState =
-      HH.div
-        [ HP.class_ $ HH.ClassName "flex items-center border-b-2" ]
+    renderSearch sst =
+      HH.div_
         [ Input.input
           ( S.getInputProps
             [ HP.placeholder "Type to search..."
-            , HP.value searchState.search
+            , HP.value sst.search
             ]
           )
         ]
 
-defaultSelectionRow :: ∀ o item source err eff m
-   . (item -> TAParentHTML o item source err eff m)
-  -> item
-  -> TAParentHTML o item source err eff m
-defaultSelectionRow renderItem item =
-  HH.li
-    [ HP.class_ $ HH.ClassName "px-4 py-1 hover:bg-grey-lighter relative" ]
-    [ HH.span_ $ [ renderItem item ]
-    , HH.span
-      [ HP.class_ $ HH.ClassName "absolute pin-t pin-b pin-r p-1 mx-3 cursor-pointer"
-      , HE.onClick (HE.input_ (TA.Remove item)) ]
-      [ HH.text "×" ]
-    ]
-
-type RenderContainerRow o item
-   = Maybe Int
-  -> Int
-  -> Fuzzy item
-  -> H.HTML Void (C.ContainerQuery o (Fuzzy item))
-
-defaultContainerRow :: ∀ o item
-   . (Fuzzy item -> Array (H.HTML Void (C.ContainerQuery o (Fuzzy item))))
-  -> RenderContainerRow o item
-defaultContainerRow renderFuzzy highlightIndex itemIndex item =
-  HH.li
-    ( C.getItemProps itemIndex
-      [ HP.class_ $ HH.ClassName $ "px-4 py-1 text-grey-darkest" <> hover ]
-    )
-    ( renderFuzzy item )
-  where
-    hover = if highlightIndex == Just itemIndex then " bg-grey-lighter" else ""
-
--- Takes a key to the segment that you want to display highlighted.
--- WARN: If the key you provided does not exist in the map, your item will not be
--- rendered!
-boldMatches :: ∀ item i p. String -> Fuzzy item -> Array (H.HTML i p)
-boldMatches key (Fuzzy { segments }) = boldMatch <$> (fromMaybe [ Left key ] $ lookup key segments)
-  where
-    boldMatch (Left str) = HH.text str
-    boldMatch (Right str) = HH.span [ HP.class_ $ HH.ClassName "font-bold" ] [ HH.text str ]
