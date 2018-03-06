4ffffc2b0cb82d07ee309e97e7967921e2d97507
Chris Cornwell <chris@citizennet.com>
2018-03-05 19:10:05 -0800

    remove labels from range slider, add bg-transparent to global range selector FBCM-2216

--- src/Blocks/Range.purs
+++ src/Blocks/Range.purs
@@ -6,30 +6,9 @@ import DOM.HTML.Indexed (HTMLinput)
 import Halogen.HTML as HH
 import Halogen.HTML.Properties as HP
 
-type RangeConfig =
-  { min :: String
-  , max :: String
-  }
-
 range
   :: ∀ p i
-   . RangeConfig
-  -> Array (HH.IProp HTMLinput i)
+   . Array (HH.IProp HTMLinput i)
   -> HH.HTML p i
-range { min, max } iprops =
-  HH.div
-    [ HP.class_ $ HH.ClassName "flex items-center w-full" ]
-    [ HH.span
-      [ HP.class_ $ HH.ClassName "flex-no-grow mr-4" ]
-      [ HH.text min ]
-    , HH.input
-      ( iprops <>
-        [ HP.type_ HP.InputRange
-        , HP.class_ $ HH.ClassName "bg-transparent flex-1"
-        ]
-      )
-    , HH.span
-      [ HP.class_ $ HH.ClassName "flex-no-grow ml-4" ]
-      [ HH.text max ]
-    ]
-
+range iprops =
+  HH.input $ iprops <> [ HP.type_ HP.InputRange ]
--- ui-guide/Components/FormControl.purs
+++ ui-guide/Components/FormControl.purs
@@ -156,7 +156,6 @@ component =
           [ Component.component
               { title: "Range" }
               [ Range.range
-                { min: "Min Label", max: "Max Label" }
                 [ HP.id_ "range_"
                 , HP.min 0.0
                 , HP.max 100.0
@@ -171,7 +170,6 @@ component =
                 , inputId: "range"
                 }
                 ( Range.range
-                    { min: "A Lot", max: "All Of Them" }
                     [ HP.id_ "range"
                     , HP.min 0.0
                     , HP.max 100.0
