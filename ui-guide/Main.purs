99bc07f8bc947d2cfe280f0c2dee4404d0ccd263
Thomas R. Honeyman <admin@thomashoneyman.com>
2018-04-21 13:50:43 -0700

    Update to main.

--- ui-guide/Main.purs
+++ ui-guide/Main.purs
@@ -5,12 +5,10 @@ import Prelude
 import Control.Monad.Aff.Console (CONSOLE)
 import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Timer (TIMER)
-import Debug.Trace (traceAnyA)
 import Halogen.Aff as HA
 import Network.HTTP.Affjax (AJAX)
 import UIGuide.App (runStorybook)
 import UIGuide.App.Routes (routes, groups)
-import UIGuide.Components.Validation (badEmail, badPass, good, runSignupForm)
 
 type Effects =
   ( ajax :: AJAX
@@ -18,13 +16,7 @@ type Effects =
   , timer :: TIMER
   )
 
---  main :: Eff (HA.HalogenEffects Effects) Unit
---  main = HA.runHalogenAff do
---    body <- HA.awaitBody
---    runStorybook routes groups body
-
-main :: Eff _ Unit
-main = do
-  traceAnyA =<< runSignupForm badPass
-  traceAnyA =<< runSignupForm badEmail
-  traceAnyA =<< runSignupForm good
+main :: Eff (HA.HalogenEffects Effects) Unit
+main = HA.runHalogenAff do
+  body <- HA.awaitBody
+  runStorybook routes groups body
