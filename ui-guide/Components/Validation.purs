7ad2a94c0b44e14ff1180f9db98c522118ff7f68
Thomas R. Honeyman <admin@thomashoneyman.com>
2018-04-23 18:06:10 -0700

    Comment out validation.

--- ui-guide/Components/Validation.purs
+++ ui-guide/Components/Validation.purs
@@ -12,62 +12,62 @@ import Polyform.Validation (hoistFnV)
 ----------
 -- Form
 
--- These fields are available in our form
-_password = SProxy :: SProxy "password"
-_email = SProxy :: SProxy "email"
-
-signupForm = { password: _, email: _ }
-  <$> formFromField _password (hoistFnV validateNonEmptyStr)
-  <*> formFromField _email
-      ( hoistFnV validateNonEmptyStr
-        >>>
-        hoistFnV (validateStrIsEmail "Not an email")
-      )
-
-runSignupForm =
-  runForm
-    signupForm
-    { password:
-      { validated: Right ""
-      , setValue: inj _password
-      , setValidate: inj _password
-      }
-    , email:
-      { validated: Right ""
-      , setValue: inj _email
-      , setValidate: inj _password
-      }
-    }
-
-badEmail =
-  { password:
-    { value: "longenough89"
-    , shouldValidate: true
-    }
-  , email:
-    { value: "notlongenough89"
-    , shouldValidate: false
-    }
-  }
-
-badPass =
-  { password:
-    { value: ""
-    , shouldValidate: true
-    }
-  , email:
-    { value: "thomas@thomas.thomas"
-    , shouldValidate: false
-    }
-  }
-
-good =
-  { password:
-    { value: "longenough90"
-    , shouldValidate: true
-    }
-  , email:
-    { value: "thomas@thomas.thomas"
-    , shouldValidate: false
-    }
-  }
+--  -- These fields are available in our form
+--  _password = SProxy :: SProxy "password"
+--  _email = SProxy :: SProxy "email"
+--
+--  signupForm = { password: _, email: _ }
+--    <$> formFromField _password (hoistFnV validateNonEmptyStr)
+--    <*> formFromField _email
+--        ( hoistFnV validateNonEmptyStr
+--          >>>
+--          hoistFnV (validateStrIsEmail "Not an email")
+--        )
+--
+--  runSignupForm =
+--    runForm
+--      signupForm
+--      { password:
+--        { validated: Right ""
+--        , setValue: inj _password
+--        , setValidate: inj _password
+--        }
+--      , email:
+--        { validated: Right ""
+--        , setValue: inj _email
+--        , setValidate: inj _password
+--        }
+--      }
+--
+--  badEmail =
+--    { password:
+--      { value: "longenough89"
+--      , shouldValidate: true
+--      }
+--    , email:
+--      { value: "notlongenough89"
+--      , shouldValidate: false
+--      }
+--    }
+--
+--  badPass =
+--    { password:
+--      { value: ""
+--      , shouldValidate: true
+--      }
+--    , email:
+--      { value: "thomas@thomas.thomas"
+--      , shouldValidate: false
+--      }
+--    }
+--
+--  good =
+--    { password:
+--      { value: "longenough90"
+--      , shouldValidate: true
+--      }
+--    , email:
+--      { value: "thomas@thomas.thomas"
+--      , shouldValidate: false
+--      }
+--    }
