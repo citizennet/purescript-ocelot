2a4cb7025004cfab309af7cf02a883dcbb50a72d
Thomas R. Honeyman <admin@thomashoneyman.com>
2018-06-15 13:51:25 -0700

    Update tests to 0.12, too.

--- test/Main.purs
+++ test/Main.purs
@@ -2,32 +2,21 @@ module Test.Main where
 
 import Prelude
 
-import Effect.Aff.AVar (AVAR)
 import Effect (Effect)
-import Control.Monad.Eff.Console (CONSOLE)
 import Data.Maybe (Maybe)
-import Debug.Trace (traceAnyA)
 import Ocelot.Data.Record (makeDefaultFormInputs)
+import Debug.Trace (traceM)
 import Test.Unit (suite)
-import Test.Unit.Console (TESTOUTPUT)
 import Test.Unit.Main (runTest)
 import Type.Prelude (RProxy(..))
-
-type Effects eff =
-  ( console :: CONSOLE
-  , testOutput :: TESTOUTPUT
-  , avar :: AVAR
-  | eff
-  )
 
 type Fields =
   ( email :: String
   , password :: Maybe String
   )
 
-main :: ∀ eff. Eff (Effects eff) Unit
+main :: Effect Unit
 main = runTest do
   suite "Validation" do
-    --  traceAnyA $ makeDefaultFormFields (RProxy :: RProxy Fields)
-    traceAnyA $ makeDefaultFormInputs (RProxy :: RProxy Fields)
+    traceM $ makeDefaultFormInputs (RProxy :: RProxy Fields)
     pure unit
