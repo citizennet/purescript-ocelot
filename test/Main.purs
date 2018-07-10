f7926ebe05de7ae16359f9cb4f9fa89ecd612de4
David Zuch <dzuch@citizennet.com>
2018-07-09 17:48:07 -0700

    Cleaned up tests

--- test/Main.purs
+++ test/Main.purs
@@ -13,7 +13,6 @@ import Data.Tuple (Tuple(..))
 import Debug.Trace (traceM)
 import Ocelot.Data.Record (makeDefaultFormInputs)
 import Ocelot.HTML.Properties (appendIProps, css, extract)
-import Halogen.HTML as HH
 import Test.Unit (suite, test)
 import Test.Unit.Assert (equal)
 import Test.Unit.Console (log)
@@ -27,9 +26,9 @@ type Fields =
 
 main :: Effect Unit
 main = runTest do
-  -- suite "Validation" do
-    -- traceM $ makeDefaultFormInputs (RProxy :: RProxy Fields)
-    -- pure unit
+  suite "Validation" do
+    traceM $ makeDefaultFormInputs (RProxy :: RProxy Fields)
+    pure unit
   suite "HTML.Properties" do
     test "appendIProps" do
       let ipropsA = [ css "m-10 p-10 px-5 pb-12 min-w-80 w-full shadow overflow-hidden" ]
@@ -51,14 +50,14 @@ main = runTest do
 
     -- naive appendIProps:
     -- 5 tests at 100,000 computations took
-    -- 19940.0, 16041.0, 16339.0, 15695.0, 15185.0
+    -- 19940, 16041, 16339, 15695, 15185
     -- milliseconds to run
     -- 5 tests at 10,000 computations took
-    -- 2270.0, 1960.0, 1804.0, 1600.0, 1598.0
+    -- 2270, 1960, 1804, 1600, 1598
     -- milliseconds to run
     test "appendIProps profile" do
       start <- liftEffect now
-      _ <- log $ "Start time: " <> ( show start )
+      _ <- log $ "Start time: " <> show start
       let ipropsA = [ css "m-10 p-10 px-5 pb-12 min-w-80 w-full shadow overflow-hidden" ]
           ipropsB = [ css "p-5 pb-10 min-w-100 overflow-auto h-full" ]
           go x acc
@@ -66,7 +65,7 @@ main = runTest do
             | otherwise  = go (x - 1) $ appendIProps ipropsA ipropsB
           run = go 10000 []
       end <- liftEffect now
-      _ <- log $ "End time: " <> ( show end )
+      _ <- log $ "End time: " <> show end
       let (Milliseconds start') = unInstant start
           (Milliseconds end')   = unInstant end
-      log $ "Total ellapsed time: " <> ( show $ end' - start' )
+      log $ "Total ellapsed time: (Milliseconds " <> ( show $ end' - start' ) <> ")"
