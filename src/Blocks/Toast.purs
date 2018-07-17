beb4a3a9a4255b0365a1d179ad010c76296b6c02
Thomas R. Honeyman <admin@thomashoneyman.com>
2018-07-17 16:46:17 -0700

    Add toast delay

--- src/Blocks/Toast.purs
+++ src/Blocks/Toast.purs
@@ -3,47 +3,89 @@ module Ocelot.Block.Toast where
 import Prelude
 
 import DOM.HTML.Indexed (HTMLdiv)
+import Data.Array (snoc)
+import Data.Bifunctor (lmap, rmap)
+import Data.Foldable (foldr)
+import Data.Tuple (Tuple(..))
 import Halogen.HTML as HH
+import Halogen.HTML.Core as HC
 import Halogen.HTML.Properties as HP
 import Ocelot.HTML.Properties ((<&>))
+import Unsafe.Coerce (unsafeCoerce)
+
+type Toast r = ( open :: Boolean | r )
+
+type HTMLtoast = Toast HTMLdiv
+
+open :: ∀ r i. Boolean -> HP.IProp ( open :: Boolean | r ) i
+open = HP.prop (HH.PropName "open")
 
 -- Necessary for centering the toast
 toastContainerClasses :: Array HH.ClassName
 toastContainerClasses = HH.ClassName <$>
   [ "flex"
+  , "transition-1/4-in"
+  , "transition-1/2-out"
   , "items-center"
   , "fixed"
-  , "pin-b"
   , "pin-l"
   , "pin-r"
+  , "pin-b"
   ]
+
+containerOpenClasses :: Array HH.ClassName
+containerOpenClasses = HH.ClassName <$>
+  [ "mb-8" ]
+
+containerClosedClasses :: Array HH.ClassName
+containerClosedClasses = HH.ClassName <$>
+  [ "-mb-40" ]
 
 toastClasses :: Array HH.ClassName
 toastClasses = HH.ClassName <$>
-  [ "transition-1/4-in"
-  , "transition-1/2-out"
-  , "shadow-md"
+  [ "shadow-md"
   , "p-4"
   , "ml-auto"
   , "mr-auto"
-  , "mb-8"
-  , "flex"
   , "items-center"
   , "border"
   , "border-grey-80"
   , "bg-white"
   , "rounded"
+  , "flex"
   ]
 
 toast
   :: ∀ p i
-   . Array (HH.IProp HTMLdiv i)
+   . Array (HH.IProp HTMLtoast i)
   -> Array (HH.HTML p i)
   -> HH.HTML p i
 toast iprops html =
   HH.div
-  [ HP.classes toastContainerClasses ]
+  [ HP.classes $ toastContainerClasses <> containerClasses' ]
   [ HH.div
-    ( [ HP.classes toastClasses ] <&> iprops )
+    ( [ HP.classes toastClasses ] <&> iprops' )
     html
   ]
+  where
+    Tuple open iprops' = pullOpenProp iprops
+    containerClasses' =
+      if open
+        then containerOpenClasses
+        else containerClosedClasses
+
+pullOpenProp
+  :: ∀ r i
+   . Array (HH.IProp ( open :: Boolean | r ) i)
+  -> Tuple Boolean (Array (HH.IProp r i))
+pullOpenProp = foldr f (Tuple false [])
+  where
+    f (HP.IProp (HC.Property "open" x)) =
+      lmap $ const $ coerceExpanded x
+    f iprop = rmap $ (flip snoc) $ coerceR iprop
+
+    coerceExpanded :: HC.PropValue -> Boolean
+    coerceExpanded = unsafeCoerce
+
+    coerceR :: HH.IProp ( open :: Boolean | r ) i -> HH.IProp r i
+    coerceR = unsafeCoerce
--- ui-guide/Components/Toast.purs
+++ ui-guide/Components/Toast.purs
@@ -2,15 +2,19 @@ module UIGuide.Components.Toast where
 
 import Prelude
 
+import Data.Generic.Rep (class Generic)
+import Data.Generic.Rep.Show (genericShow)
 import Data.Maybe (Maybe(..))
+import Data.Time.Duration (Milliseconds(..))
+import Effect.Aff (delay)
 import Effect.Aff.Class (class MonadAff)
 import Halogen as H
 import Halogen.HTML as HH
-import Halogen.HTML.Events (input_, onClick) as HE
-import Ocelot.Block.Button (button) as Button
-import Ocelot.Block.Format (caption_) as Format
+import Halogen.HTML.Events as HE
+import Ocelot.Block.Button as Button
+import Ocelot.Block.Format as Format
 import Ocelot.Block.Icon as Icon
-import Ocelot.Block.Toast (toast) as Toast
+import Ocelot.Block.Toast as Toast
 import Ocelot.HTML.Properties (css)
 import UIGuide.Block.Backdrop as Backdrop
 import UIGuide.Block.Documentation as Documentation
@@ -25,7 +29,10 @@ data ToastType
   = Success
   | Error
   --  | Info
+derive instance genericToastType :: Generic ToastType _
 derive instance eqToastType :: Eq ToastType
+instance showToastType :: Show ToastType where
+  show = genericShow
 
 type Input = Unit
 
@@ -46,7 +53,9 @@ component =
   where
     eval :: Query ~> H.ComponentDSL State Query Message m
     eval (Toggle t a) = do
-      H.modify_ _ { toast = Just t }
+      st <- H.modify _ { toast = Just t }
+      H.liftAff $ delay $ Milliseconds 3000.0
+      H.modify_ _ { toast = Nothing }
       pure a
 
     render :: State -> H.ComponentHTML Query
@@ -65,16 +74,13 @@ component =
                 , Button.button
                   [ HE.onClick $ HE.input_ $ Toggle Success ]
                   [ HH.text "Success" ]
-                , if state.toast == Just Success
-                    then
-                      Toast.toast
-                      []
-                      [ Icon.success
-                        [ css "text-green text-2xl mr-2" ]
-                      , HH.p_
-                        [ HH.text "Campaign saved." ]
-                      ]
-                    else HH.text ""
+                , Toast.toast
+                  [ Toast.open $ state.toast == Just Success ]
+                  [ Icon.success
+                    [ css "text-green text-2xl mr-2" ]
+                  , HH.p_
+                    [ HH.text "Campaign saved." ]
+                  ]
                 ]
               ]
             ]
@@ -87,16 +93,13 @@ component =
                 , Button.button
                   [ HE.onClick $ HE.input_ $ Toggle Error ]
                   [ HH.text "Error" ]
-                , if state.toast == Just Error
-                    then
-                      Toast.toast
-                      []
-                      [ Icon.error
-                        [ css "text-red text-2xl mr-2" ]
-                      , HH.p_
-                        [ HH.text "Fix errors before saving." ]
-                      ]
-                    else HH.text ""
+                , Toast.toast
+                  [ Toast.open $ state.toast == Just Error ]
+                  [ Icon.error
+                    [ css "text-red text-2xl mr-2" ]
+                  , HH.p_
+                    [ HH.text "Fix errors before saving." ]
+                  ]
                 ]
               ]
             ]
