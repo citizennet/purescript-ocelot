44a132216696dbd88a72b5bf9fce8beb7c8318f6
boygao1992 <wg2313@columbia.edu>
2021-03-22 16:48:20 -0400

    experimenting with SVG solution
    The better I position these moving components (background track, intervals and
    points on the track) by css to align with the design, the stronger I feel we
    need finest control at the pixel level. Then why not use SVG?
    
    After experimenting with halogen-svg-elems, looks like we have everything we
    need except drop shadow around thumb which requires definition of filter but
    halogen-svg-elems doesn't have nodes for <def>, <filter>, <feDropShadow> yet.
    
    The minor difference in styling is acceptable for the initial run and SVG does a
    better job than CSS in positioning (given my current knowledge on both), I'm
    going to use SVG as the rendering solution for slider.
    
    Side note: If I have better knowledge of d3 and more importantly if there's a
    mature d3v6 wrapper in PS, d3 might be the best solution. Without a decent
    wrapper, it's going to be a huge pain typing all event handlers with FFI.

--- ui-guide/Components/Diagram.purs
+++ ui-guide/Components/Diagram.purs
@@ -4,6 +4,8 @@ import Prelude
 
 import Halogen as Halogen
 import Halogen.HTML as Halogen.HTML
+import Halogen.Svg.Attributes as Halogen.Svg.Attributes
+import Halogen.Svg.Elements as Halogen.Svg.Elements
 import Ocelot.Block.Card as Card
 import Ocelot.Block.Diagram as Ocelot.Block.Diagram
 import Ocelot.HTML.Properties as Ocelot.HTML.Properties
@@ -75,6 +77,128 @@ render _ =
               [ Halogen.HTML.text (show percentB <> "%")]
             ]
           ]
+        , Card.card_
+          [ Halogen.HTML.p
+            [ Ocelot.HTML.Properties.css "flex items-center" ]
+            [ Halogen.Svg.Elements.svg
+              [ Halogen.Svg.Attributes.width 400.0
+              , Halogen.Svg.Attributes.viewBox 0.0 0.0 viewBoxWidth viewBoxHeight
+              ]
+              [ Halogen.Svg.Elements.g
+                [ Halogen.Svg.Attributes.transform
+                  [ Halogen.Svg.Attributes.Translate margin margin ]
+                ]
+                [ Halogen.Svg.Elements.g
+                  [ Halogen.Svg.Attributes.transform
+                    [ Halogen.Svg.Attributes.Translate thumbRadius (betweenTopAndThumb + thumbRadius - trackRadius) ]
+                  ]
+                  [ Halogen.Svg.Elements.rect
+                    [ Halogen.Svg.Attributes.fill (pure (Halogen.Svg.Attributes.RGB 229 229 229))
+                    , Halogen.Svg.Attributes.height trackHeight
+                    , Halogen.Svg.Attributes.width trackWidth
+                    ]
+                  , Halogen.Svg.Elements.rect
+                    [ Halogen.Svg.Attributes.fill (pure (Halogen.Svg.Attributes.RGB 126 135 148))
+                    , Halogen.Svg.Attributes.height trackHeight
+                    , Halogen.Svg.Attributes.width (trackWidth * (percentB - percentA) / 100.0)
+                    , Halogen.Svg.Attributes.transform
+                      [ Halogen.Svg.Attributes.Translate (trackWidth * percentA / 100.0) 0.0 ]
+                    ]
+                  ]
+                , Halogen.Svg.Elements.g
+                  [ Halogen.Svg.Attributes.transform
+                    [ Halogen.Svg.Attributes.Translate thumbRadius (betweenTopAndThumb + thumbRadius) ]
+                  ]
+                  [ Halogen.Svg.Elements.circle
+                    [ Halogen.Svg.Attributes.r 12.5
+                    , Halogen.Svg.Attributes.fill (pure (Halogen.Svg.Attributes.RGB 255 255 255))
+                    , Halogen.Svg.Attributes.stroke (pure (Halogen.Svg.Attributes.RGB 0 0 0))
+                    , Halogen.Svg.Attributes.transform
+                      [ Halogen.Svg.Attributes.Translate (trackWidth * percentA / 100.0) 0.0 ]
+                    ]
+                  , Halogen.Svg.Elements.circle
+                    [ Halogen.Svg.Attributes.r 12.5
+                    , Halogen.Svg.Attributes.fill (pure (Halogen.Svg.Attributes.RGB 255 255 255))
+                    , Halogen.Svg.Attributes.stroke (pure (Halogen.Svg.Attributes.RGB 0 0 0))
+                    , Halogen.Svg.Attributes.transform
+                      [ Halogen.Svg.Attributes.Translate (trackWidth * percentB / 100.0) 0.0 ]
+                    ]
+                  ]
+                , Halogen.Svg.Elements.g
+                  [ Halogen.Svg.Attributes.transform
+                    [ Halogen.Svg.Attributes.Translate thumbRadius (betweenTopAndThumb + thumbRadius * 2.0 + betweenThumbAndAxis) ]
+                  ]
+                  [ Halogen.Svg.Elements.text
+                    [ Halogen.Svg.Attributes.text_anchor Halogen.Svg.Attributes.AnchorMiddle
+                    , Halogen.Svg.Attributes.transform
+                      [ Halogen.Svg.Attributes.Translate 0.0 0.0 ]
+                    ]
+                    [ Halogen.HTML.text "0%" ]
+                  , Halogen.Svg.Elements.text
+                    [ Halogen.Svg.Attributes.text_anchor Halogen.Svg.Attributes.AnchorMiddle
+                    , Halogen.Svg.Attributes.transform
+                      [ Halogen.Svg.Attributes.Translate 40.0 0.0 ]
+                    ]
+                    [ Halogen.HTML.text "1%" ]
+                  , Halogen.Svg.Elements.text
+                    [ Halogen.Svg.Attributes.text_anchor Halogen.Svg.Attributes.AnchorMiddle
+                    , Halogen.Svg.Attributes.transform
+                      [ Halogen.Svg.Attributes.Translate 80.0 0.0 ]
+                    ]
+                    [ Halogen.HTML.text "2%" ]
+                  , Halogen.Svg.Elements.text
+                    [ Halogen.Svg.Attributes.text_anchor Halogen.Svg.Attributes.AnchorMiddle
+                    , Halogen.Svg.Attributes.transform
+                      [ Halogen.Svg.Attributes.Translate 120.0 0.0 ]
+                    ]
+                    [ Halogen.HTML.text "3%" ]
+                  , Halogen.Svg.Elements.text
+                    [ Halogen.Svg.Attributes.text_anchor Halogen.Svg.Attributes.AnchorMiddle
+                    , Halogen.Svg.Attributes.transform
+                      [ Halogen.Svg.Attributes.Translate 160.0 0.0 ]
+                    ]
+                    [ Halogen.HTML.text "4%" ]
+                  , Halogen.Svg.Elements.text
+                    [ Halogen.Svg.Attributes.text_anchor Halogen.Svg.Attributes.AnchorMiddle
+                    , Halogen.Svg.Attributes.transform
+                      [ Halogen.Svg.Attributes.Translate 200.0 0.0 ]
+                    ]
+                    [ Halogen.HTML.text "5%" ]
+                  , Halogen.Svg.Elements.text
+                    [ Halogen.Svg.Attributes.text_anchor Halogen.Svg.Attributes.AnchorMiddle
+                    , Halogen.Svg.Attributes.transform
+                      [ Halogen.Svg.Attributes.Translate 240.0 0.0 ]
+                    ]
+                    [ Halogen.HTML.text "6%" ]
+                  , Halogen.Svg.Elements.text
+                    [ Halogen.Svg.Attributes.text_anchor Halogen.Svg.Attributes.AnchorMiddle
+                    , Halogen.Svg.Attributes.transform
+                      [ Halogen.Svg.Attributes.Translate 280.0 0.0 ]
+                    ]
+                    [ Halogen.HTML.text "7%" ]
+                  , Halogen.Svg.Elements.text
+                    [ Halogen.Svg.Attributes.text_anchor Halogen.Svg.Attributes.AnchorMiddle
+                    , Halogen.Svg.Attributes.transform
+                      [ Halogen.Svg.Attributes.Translate 320.0 0.0 ]
+                    ]
+                    [ Halogen.HTML.text "8%" ]
+                  , Halogen.Svg.Elements.text
+                    [ Halogen.Svg.Attributes.text_anchor Halogen.Svg.Attributes.AnchorMiddle
+                    , Halogen.Svg.Attributes.transform
+                      [ Halogen.Svg.Attributes.Translate 360.0 0.0 ]
+                    ]
+                    [ Halogen.HTML.text "9%" ]
+                  , Halogen.Svg.Elements.text
+                    [ Halogen.Svg.Attributes.text_anchor Halogen.Svg.Attributes.AnchorMiddle
+                    , Halogen.Svg.Attributes.transform
+                      [ Halogen.Svg.Attributes.Translate 400.0 0.0 ]
+                    ]
+                    [ Halogen.HTML.text "10%" ]
+                  ]
+                ]
+              ]
+            ]
+          ]
         ]
       ]
     ]
@@ -85,3 +209,34 @@ render _ =
 
   percentB :: Number
   percentB = 100.0
+
+  -- | NOTE assume thumbRadius > trackRadius
+  viewBoxHeight :: Number
+  viewBoxHeight = margin + betweenTopAndThumb + thumbRadius * 2.0 + betweenThumbAndAxis + axisHeight + margin
+
+  viewBoxWidth :: Number
+  viewBoxWidth = margin + thumbRadius + trackWidth + thumbRadius + margin
+
+  margin :: Number
+  margin = 5.0
+
+  betweenTopAndThumb :: Number
+  betweenTopAndThumb = 20.0
+
+  betweenThumbAndAxis :: Number
+  betweenThumbAndAxis = 20.0
+
+  axisHeight :: Number
+  axisHeight = 20.0
+
+  thumbRadius :: Number
+  thumbRadius = 10.0
+
+  trackWidth :: Number
+  trackWidth = 400.0
+
+  trackHeight :: Number
+  trackHeight = trackRadius * 2.0
+
+  trackRadius :: Number
+  trackRadius = 2.5
