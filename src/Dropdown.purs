c460b8cc64b54ef1447006b04016fb5ff3368aec
David <dzuch@citizennet.com>
2021-04-26 15:56:09 -0700

    Sort Dropdown types and values
    Before changing anything, we want to organize the contents of
    `Ocelot.Dropdown` to make it easier to navigate.

--- src/Dropdown.purs
+++ src/Dropdown.purs
@@ -13,65 +13,87 @@ import Renderless.State (modifyStore_)
 import Select as S
 import Type.Data.Symbol (SProxy(..))
 
+--------
+-- Types
+
+data Action item m
+  = PassingOutput (Output item)
+  | ReceiveRender (Input item m)
+
+type ChildSlots item 
+  = ( select :: S.Slot (Query item) EmbeddedChildSlots (Output item) Unit
+    )
+
+type Component item m 
+  = H.Component HH.HTML (Query item) (Input item m) (Output item) m
+
+type ComponentHTML item m 
+  = H.ComponentHTML (Action item m) (ChildSlots item) m
+
+type ComponentM item m a 
+  = H.HalogenM (StateStore item m) (Action item m) (ChildSlots item) (Output item) m a
+
+type ComponentRender item m 
+  = State item -> ComponentHTML item m
+
+type CompositeAction
+  = S.Action EmbeddedAction
+
+type CompositeComponent item m 
+  = H.Component HH.HTML (CompositeQuery item) (CompositeInput item) (Output item) m
+
+type CompositeComponentHTML m 
+  = H.ComponentHTML CompositeAction EmbeddedChildSlots m
+
+type CompositeComponentM item m a 
+  = H.HalogenM (CompositeState item) CompositeAction EmbeddedChildSlots (Output item) m a
+
+type CompositeComponentRender item m 
+  = (CompositeState item) -> CompositeComponentHTML m
+
+type CompositeInput item 
+  = S.Input (StateRow item)
+
+type CompositeQuery item 
+  = S.Query (Query item) EmbeddedChildSlots
+
+type CompositeState item 
+  = S.State (StateRow item)
+
+type EmbeddedAction = Void
+
+type EmbeddedChildSlots = () -- NOTE no extension
+
+type Input item m =
+  { selectedItem :: Maybe item
+  , items :: Array item
+  , render :: CompositeComponentRender item m
+  }
+
+data Output item
+  = Selected item
+  | VisibilityChanged S.Visibility
+
+data Query item a
+  = SetItems (Array item) a
+  | SetSelection (Maybe item) a
 
 type Slot item id = H.Slot (Query item) (Output item) id
 
-type Component item m = H.Component HH.HTML (Query item) (Input item m) (Output item) m
-type ComponentHTML item m = H.ComponentHTML (Action item m) (ChildSlots item) m
-type ComponentRender item m = State item -> ComponentHTML item m
-type ComponentM item m a = H.HalogenM (StateStore item m) (Action item m) (ChildSlots item) (Output item) m a
+type Spec item m 
+  = S.Spec (StateRow item) (Query item) EmbeddedAction EmbeddedChildSlots (CompositeInput item) (Output item) m
+
+type State item = Record (StateRow item)
 
 type StateRow item =
   ( selectedItem :: Maybe item
   , items :: Array item
   )
 
-type State item = Record (StateRow item)
-
 type StateStore item m = Store (State item) (ComponentHTML item m)
 
-type Input item m =
-  { selectedItem :: Maybe item
-  , items :: Array item
-
-  , render :: CompositeComponentRender item m
-  }
-
-data Action item m
-  = PassingOutput (Output item)
-  | ReceiveRender (Input item m)
-
-type EmbeddedAction = Void
-
-data Query item a
-  = SetItems (Array item) a
-  | SetSelection (Maybe item) a
-
-data Output item
-  = Selected item
-  | VisibilityChanged S.Visibility
-
-type ChildSlots item =
-  ( select :: S.Slot (Query item) EmbeddedChildSlots (Output item) Unit
-  )
-_select = SProxy :: SProxy "select"
-
-
-type CompositeState item = S.State (StateRow item)
-type CompositeAction = S.Action EmbeddedAction
-type CompositeQuery item = S.Query (Query item) EmbeddedChildSlots
-type CompositeInput item = S.Input (StateRow item)
-type EmbeddedChildSlots = () -- NOTE no extension
-
-type Spec item m = S.Spec (StateRow item) (Query item) EmbeddedAction EmbeddedChildSlots (CompositeInput item) (Output item) m
-type CompositeComponent item m = H.Component HH.HTML (CompositeQuery item) (CompositeInput item) (Output item) m
-type CompositeComponentHTML m = H.ComponentHTML CompositeAction EmbeddedChildSlots m
-type CompositeComponentRender item m = (CompositeState item) -> CompositeComponentHTML m
-type CompositeComponentM item m a = H.HalogenM (CompositeState item) CompositeAction EmbeddedChildSlots (Output item) m a
-
-
 ------------
--- Container
+-- Component
 
 component
   :: forall item m
@@ -85,6 +107,65 @@ component = H.mkComponent
       , handleQuery = handleQuery
       }
   }
+
+---------
+-- Values
+
+_select = SProxy :: SProxy "select"
+
+-- Embedded > handleMessage
+embeddedHandleMessage
+  :: forall item m. MonadAff m => S.Event -> CompositeComponentM item m Unit
+embeddedHandleMessage = case _ of
+  S.Selected idx -> do
+    { items } <- H.get
+    case items !! idx of
+      Nothing -> pure unit
+      Just item -> do
+        H.modify_
+          _ { visibility = S.Off
+            , selectedItem = Just item
+            }
+        H.raise $ Selected item
+  S.VisibilityChanged vis -> H.raise (VisibilityChanged vis)
+  _ -> pure unit
+
+-- Embedded > handleQuery
+embeddedHandleQuery
+  :: forall item m a. MonadAff m => Query item a -> CompositeComponentM item m (Maybe a)
+embeddedHandleQuery = case _ of
+  SetItems items a -> Just a <$ do
+    H.modify_ _ { items = items }
+  SetSelection item a -> Just a <$ do
+    H.modify_ _ { selectedItem = item }
+
+-- NOTE configure Select
+embeddedInput :: forall item. State item -> CompositeInput item
+embeddedInput { selectedItem, items } =
+  { inputType: S.Toggle
+  , search: Nothing
+  , debounceTime: Nothing
+  , getItemCount: Array.length <<< _.items
+  , selectedItem
+  , items
+  }
+
+-- NOTE re-raise output messages from the embedded component
+-- NOTE update Dropdown render function if it relies on external state
+handleAction :: forall item m. MonadAff m => Action item m -> ComponentM item m Unit
+handleAction = case _ of
+  PassingOutput output ->
+    H.raise output
+  ReceiveRender { render } -> do
+    modifyStore_ (renderAdapter render) identity
+
+-- NOTE passing query to the embedded component
+handleQuery :: forall item m a. Query item a -> ComponentM item m (Maybe a)
+handleQuery = case _ of
+  SetItems items a -> Just a <$ do
+    H.query _select unit (S.Query $ H.tell $ SetItems items)
+  SetSelection item a -> Just a <$ do
+    H.query _select unit (S.Query $ H.tell $ SetSelection item)
 
 initialState :: forall item m. MonadAff m => Input item m -> StateStore item m
 initialState { render, selectedItem, items } =
@@ -111,67 +192,3 @@ spec embeddedRender =
   , handleQuery = embeddedHandleQuery
   , handleEvent = embeddedHandleMessage
   }
-
--- NOTE configure Select
-embeddedInput :: forall item. State item -> CompositeInput item
-embeddedInput { selectedItem, items } =
-  { inputType: S.Toggle
-  , search: Nothing
-  , debounceTime: Nothing
-  , getItemCount: Array.length <<< _.items
-
-  , selectedItem
-  , items
-  }
-
--- NOTE re-raise output messages from the embedded component
--- NOTE update Dropdown render function if it relies on external state
-handleAction :: forall item m. MonadAff m => Action item m -> ComponentM item m Unit
-handleAction = case _ of
-  PassingOutput output ->
-    H.raise output
-
-  ReceiveRender { render } -> do
-    modifyStore_ (renderAdapter render) identity
-
--- NOTE passing query to the embedded component
-handleQuery :: forall item m a. Query item a -> ComponentM item m (Maybe a)
-handleQuery = case _ of
-  SetItems items a -> Just a <$ do
-    H.query _select unit (S.Query $ H.tell $ SetItems items)
-
-  SetSelection item a -> Just a <$ do
-    H.query _select unit (S.Query $ H.tell $ SetSelection item)
-
--------------------------
--- Embedded > handleQuery
-
-embeddedHandleQuery
-  :: forall item m a. MonadAff m => Query item a -> CompositeComponentM item m (Maybe a)
-embeddedHandleQuery = case _ of
-  SetItems items a -> Just a <$ do
-    H.modify_ _ { items = items }
-
-  SetSelection item a -> Just a <$ do
-    H.modify_ _ { selectedItem = item }
-
----------------------------
--- Embedded > handleMessage
-
-embeddedHandleMessage
-  :: forall item m. MonadAff m => S.Event -> CompositeComponentM item m Unit
-embeddedHandleMessage = case _ of
-  S.Selected idx -> do
-    { items } <- H.get
-    case items !! idx of
-      Nothing -> pure unit
-      Just item -> do
-        H.modify_
-          _ { visibility = S.Off
-            , selectedItem = Just item
-            }
-        H.raise $ Selected item
-
-  S.VisibilityChanged vis -> H.raise (VisibilityChanged vis)
-
-  _ -> pure unit
