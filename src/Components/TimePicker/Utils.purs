23b327b92ef1089ec52bb6d74471291029ea9d39
Gabe Johnson <gabe@citizennet.com>
2021-04-27 19:43:56 -0700

    FBCM-4154 Fix meridiem selection
    Previously, if the user didn't type a "P" or a "p" after a number in the
    time picker input, the format "HHmm" was assumed (24-hour time). This
    meant that if the user explicity typed out "12:45 AM", the value
    "12:45 PM" would result since using "HHmm", "1245" is equivalent to
    "12:45 PM". In order to specify "12:45 AM" one would have to write "045"
    or "0:45".
    
    We fix this by adding the possibility that the user intended to specify
    "AM" and the format should be "hhmm a" (12-hour time). We only fall back
    to 24-hour time if "AM" or "PM" cannot be guessed.

--- src/Components/TimePicker/Utils.purs
+++ src/Components/TimePicker/Utils.purs
@@ -2,27 +2,43 @@ module Ocelot.Components.TimePicker.Utils where
 
 import Prelude
 
+import Control.Alt ((<|>))
 import Data.Array (length)
 import Data.Array.NonEmpty (catMaybes, head)
 import Data.DateTime (time)
 import Data.Either (either, hush)
 import Data.Formatter.DateTime (unformatDateTime)
 import Data.Maybe (Maybe(..), fromMaybe, maybe)
-import Data.String (joinWith)
+import Data.String (joinWith, toLower)
 import Data.String.Regex (match, parseFlags, regex)
 import Data.Time (Time)
 
+data Meridiem
+  = AM
+  | PM
+
+meridiemToString :: Meridiem -> String
+meridiemToString = case _ of
+  AM -> "AM"
+  PM -> "PM"
 
 ----------
 -- Search
 guessTime :: String -> Maybe Time
 guessTime ""   = Nothing
 guessTime text =
-  let meridian :: Maybe String
-      meridian = either
-        (const Nothing)
-        (\r -> join $ head <$> match r text)
-        (regex "p" $ parseFlags "i")
+  let meridiem :: Maybe Meridiem
+      meridiem = do
+        let
+          regexFlags = parseFlags "i"
+        regexA <- hush (regex "a" $ regexFlags)
+        regexP <- hush (regex "p" $ regexFlags)
+        matched <- match regexA text <|> match regexP text
+        meridiemString <- head matched
+        case toLower meridiemString of
+          "a" -> Just AM
+          "p" -> Just PM
+          _ -> Nothing
 
       digits :: Array String
       digits = either
@@ -42,9 +58,13 @@ guessTime text =
         _ -> Nothing
 
       format :: String
-      format = (maybe "HHmm" (const "hhmm a") meridian)
+      format = (maybe "HHmm" (const "hhmm a") meridiem)
+
+      suffix :: String
+      suffix = maybe "" (\m -> " " <> meridiemToString m) meridiem
+
 
       guess :: Maybe String
-      guess = (_ <> maybe "" (const " PM") meridian) <$> hourMin
+      guess = (_ <> suffix) <$> hourMin
 
    in time <$> (join $ hush <<< unformatDateTime format <$> guess)
