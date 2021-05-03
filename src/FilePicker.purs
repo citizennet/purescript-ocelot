023c9f33d12b75875529d9d280868c711871ac01
boygao1992 <wg2313@columbia.edu>
2021-05-03 18:34:39 -0400

    render drag over and idle states separately
    Without "pointer-events": "none" to prevent event firing from children, when
    cursor enters children (icon, label) a DragLeave event might be fired and set
    the state back to idle.

--- src/FilePicker.purs
+++ src/FilePicker.purs
@@ -7,13 +7,22 @@ module Ocelot.FilePicker
 
 import Prelude
 
+import DOM.HTML.Indexed (HTMLdiv)
+import Data.Maybe (Maybe(..))
 import Effect.Aff.Class (class MonadAff)
+import Effect.Class.Console as Effect.Class.Console
 import Foreign.Object as Foreign.Object
 import Halogen as Halogen
 import Halogen.HTML as Halogen.HTML
+import Halogen.HTML.Events as Ocelot.HTML.Events
 import Halogen.HTML.Properties as Halogen.HTML.Properties
 import Ocelot.Block.Icon as Ocelot.Block.Icon
 import Ocelot.HTML.Properties as Ocelot.HTMl.Properties
+import Web.Event.Event as Web.Event.Event
+import Web.File.File as Web.File.File
+import Web.File.FileList as Web.File.FileList
+import Web.HTML.Event.DataTransfer as Web.HTML.Event.DataTransfer
+import Web.HTML.Event.DragEvent as Web.HTML.Event.DragEvent
 
 type Slot = Halogen.Slot Query Output
 
@@ -21,9 +30,15 @@ type Component m = Halogen.Component Halogen.HTML.HTML Query Input Output m
 type ComponentHTML m = Halogen.ComponentHTML Action ChildSlots m
 type ComponentM m a = Halogen.HalogenM State Action ChildSlots Output m a
 
-type State = Unit
+type State =
+  { dragOver :: Boolean
+  }
 
 data Action
+  = DragEnter Web.HTML.Event.DragEvent.DragEvent
+  | DragLeave Web.HTML.Event.DragEvent.DragEvent
+  | DropFile Web.HTML.Event.DragEvent.DragEvent
+  | PreventDefault Web.Event.Event.Event
 
 data Query a
 
@@ -45,10 +60,73 @@ component =
     , eval:
         Halogen.mkEval
           Halogen.defaultEval
+            { handleAction = handleAction
+            }
     }
 
 initialState :: Input -> State
-initialState input = unit
+initialState input =
+  { dragOver: false
+  }
+
+handleAction ::
+  forall m.
+  MonadAff m =>
+  Action ->
+  ComponentM m Unit
+handleAction = case _ of
+  DragEnter dragEvent -> dragEnter dragEvent
+  DragLeave dragEvent -> dragLeave dragEvent
+  DropFile dragEvent -> dropFile dragEvent
+  PreventDefault event -> preventDefault event
+
+dragEnter ::
+  forall m.
+  MonadAff m =>
+  Web.HTML.Event.DragEvent.DragEvent ->
+  ComponentM m Unit
+dragEnter dragEvent = do
+  preventDefault (Web.HTML.Event.DragEvent.toEvent dragEvent)
+  Halogen.modify_ _ { dragOver = true }
+
+dragLeave ::
+  forall m.
+  MonadAff m =>
+  Web.HTML.Event.DragEvent.DragEvent ->
+  ComponentM m Unit
+dragLeave dragEvent = do
+  preventDefault (Web.HTML.Event.DragEvent.toEvent dragEvent)
+  Halogen.modify_ _ { dragOver = false }
+
+dropFile ::
+  forall m.
+  MonadAff m =>
+  Web.HTML.Event.DragEvent.DragEvent ->
+  ComponentM m Unit
+dropFile dragEvent = do
+  preventDefault (Web.HTML.Event.DragEvent.toEvent dragEvent)
+  Halogen.modify_ _ { dragOver = false }
+  case Web.HTML.Event.DataTransfer.files (Web.HTML.Event.DragEvent.dataTransfer dragEvent) of
+    Nothing -> pure unit
+    Just fileList -> do
+      let
+        files :: Array Web.File.File.File
+        files = Web.File.FileList.items fileList
+      Halogen.liftEffect -- TODO debug
+        $ Effect.Class.Console.logShow
+        $ map Web.File.File.name
+        $ files
+      pure unit -- TODO handle file
+
+preventDefault ::
+  forall m.
+  MonadAff m =>
+  Web.Event.Event.Event ->
+  ComponentM m Unit
+preventDefault event =
+  Halogen.liftEffect do
+    Web.Event.Event.preventDefault event
+    Web.Event.Event.stopPropagation event
 
 render ::
   forall m.
@@ -56,25 +134,77 @@ render ::
   ComponentHTML m
 render state =
   Halogen.HTML.div_
-    [ renderDropBox
+    [ renderDropBox state
     , renderInput
     ]
 
 renderDropBox ::
   forall m.
+  State ->
   ComponentHTML m
-renderDropBox =
-  Halogen.HTML.div
-    [ Ocelot.HTMl.Properties.css "bg-grey-70-a40 p-10"
-    , Ocelot.HTMl.Properties.style <<< Foreign.Object.fromHomogeneous $
-        { outline: "2px dashed #8f9eb3"
-        , "outline-offset": "-10px"
-        , transition: "outline-offset .15s ease-in-out, background-color .15s linear"
-        }
-    ]
-    [ renderIcon
-    , renderLabel
-    ]
+renderDropBox state
+  | state.dragOver =
+    Halogen.HTML.div ipropDragOver
+      [ Halogen.HTML.div
+          [ Ocelot.HTMl.Properties.style <<< Foreign.Object.fromHomogeneous $
+            { "pointer-events": "none" } -- NOTE prevent event firing from children
+          ]
+          renderContent
+      ]
+  | otherwise =
+    Halogen.HTML.div ipropIdle
+      renderContent
+
+ipropIdle :: Array (Halogen.HTML.Properties.IProp HTMLdiv Action)
+ipropIdle =
+  [ Ocelot.HTMl.Properties.css cssIdle
+  , Ocelot.HTMl.Properties.style styleIdle
+  , Ocelot.HTML.Events.onDrag (Just <<< PreventDefault <<< Web.HTML.Event.DragEvent.toEvent)
+  , Ocelot.HTML.Events.onDragEnter (Just <<< DragEnter)
+  , Ocelot.HTML.Events.onDragOver (Just <<< PreventDefault <<< Web.HTML.Event.DragEvent.toEvent)
+  , Ocelot.HTML.Events.onDragStart (Just <<< PreventDefault <<< Web.HTML.Event.DragEvent.toEvent)
+  ]
+  where
+  cssIdle :: String
+  cssIdle = "bg-grey-70-a40 p-10"
+
+  styleIdle :: Foreign.Object.Object String
+  styleIdle =
+    Foreign.Object.fromHomogeneous
+      { outline: "2px dashed #8f9eb3"
+      , "outline-offset": "-10px"
+      , transition: "outline-offset .15s ease-in-out, background-color .15s linear"
+      }
+
+ipropDragOver :: Array (Halogen.HTML.Properties.IProp HTMLdiv Action)
+ipropDragOver =
+  [ Ocelot.HTMl.Properties.css cssDragOver
+  , Ocelot.HTMl.Properties.style styleDragOver
+  , Ocelot.HTML.Events.onDragEnd (Just <<< PreventDefault <<< Web.HTML.Event.DragEvent.toEvent)
+  , Ocelot.HTML.Events.onDragLeave (Just <<< DragLeave)
+  , Ocelot.HTML.Events.onDragOver (Just <<< PreventDefault <<< Web.HTML.Event.DragEvent.toEvent)
+  , Ocelot.HTML.Events.onDrop (Just <<< DropFile)
+  ]
+  where
+  cssDragOver :: String
+  cssDragOver = "bg-grey-95 p-10"
+
+  styleDragOver :: Foreign.Object.Object String
+  styleDragOver =
+    Foreign.Object.fromHomogeneous
+    { outline: "2px dashed #8f9eb3"
+    , "outline-offset": "-20px"
+    , transition: "outline-offset .15s ease-in-out, background-color .15s linear"
+    }
+
+renderContent ::
+  forall m.
+  Array (ComponentHTML m)
+renderContent =
+  [ renderIcon
+  , renderLabel
+  ]
+
 
 renderIcon ::
   forall m.
