90c017b8de1ecdc33c3289070c7fb3565f16a57c
boygao1992 <wg2313@columbia.edu>
2021-09-10 12:35:10 -0400

    render two states of clipboard button

--- src/Clipboard.purs
+++ src/Clipboard.purs
@@ -6,8 +6,13 @@ module Ocelot.Clipboard
   ) where
 
 import Prelude
+
 import Halogen as Halogen
 import Halogen.HTML as Halogen.HTML
+import Halogen.HTML.Properties as Halogen.HTML.Propetires
+import Ocelot.Block.Button as Ocelot.Block.Button
+import Ocelot.Block.Icon as Ocelot.Block.Icon
+import Ocelot.HTML.Properties as Ocelot.HTML.Properties
 
 type Slot
   = Halogen.Slot Query Output
@@ -22,7 +27,7 @@ type ComponentM m
   = Halogen.HalogenM State Action ChildSlot Output m
 
 type State
-  = {}
+  = { copied :: Boolean }
 
 data Action
 
@@ -50,10 +55,35 @@ component =
     }
 
 initialState :: Input -> State
-initialState _ = {}
+initialState _ =
+  { copied: false
+  }
 
 render ::
   forall m.
   State ->
   ComponentHTML m
-render _ = Halogen.HTML.text ""
+render state
+  | state.copied = renderDone
+  | otherwise = renderActive
+
+renderActive :: forall m. ComponentHTML m
+renderActive =
+  Ocelot.Block.Button.button
+    []
+    [ Ocelot.Block.Icon.dataSources_
+    , Halogen.HTML.span
+        [ Ocelot.HTML.Properties.css "ml-2" ]
+        [ Halogen.HTML.text "Copy to Clipboard" ]
+    ]
+
+renderDone :: forall m. ComponentHTML m
+renderDone =
+  Ocelot.Block.Button.button
+    [ Halogen.HTML.Propetires.disabled true ]
+    [ Ocelot.Block.Icon.success
+        [ Ocelot.HTML.Properties.css "text-green"]
+    , Halogen.HTML.span
+        [ Ocelot.HTML.Properties.css "ml-2" ]
+        [ Halogen.HTML.text "Copied" ]
+    ]
